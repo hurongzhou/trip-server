cbd7b13f9f1aa9d688cd9905f59d45ba09b22012
胡荣 <huronguestc@gmail.com>
2017-12-27 20:00:36 +0800

    餐馆管理员登录

--- com-trip-dao/src/main/java/com/trip/daoImpl/StoreDaoImpl.java
+++ com-trip-dao/src/main/java/com/trip/daoImpl/StoreDaoImpl.java
@@ -2,20 +2,35 @@ package com.trip.daoImpl;
 
 import com.trip.dao.StoreDao;
 import com.trip.entity.Commodity;
+import com.trip.entity.Restaurant;
 import com.trip.mapper.CommodityMapper;
+import com.trip.mapper.RestaurantMapper;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class StoreDaoImpl implements StoreDao{
 
     @Resource
     CommodityMapper commodityMapper;
+    @Resource
+    RestaurantMapper restaurantMapper;
 
     @Override
     public List<Commodity> findCommoditiesByCondition(Commodity commodityCondition) {
         return commodityMapper.findCommodityByCondition(commodityCondition);
+    }
+
+    @Override
+    public void addCommodity(Commodity commodity) {
+        commodityMapper.insertSelective(commodity);
+    }
+
+    @Override
+    public Restaurant login(Map<String, Object> param) {
+        return restaurantMapper.login(param);
     }
 }
