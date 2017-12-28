3ee6dc0f8035e5bb47401a2b3a2db171dcfa43b2
胡荣 <huronguestc@gmail.com>
2017-12-28 20:45:53 +0800

    add Seriable

--- com-trip-service/src/main/java/com/trip/service/DishesService.java
+++ com-trip-service/src/main/java/com/trip/service/DishesService.java
@@ -5,6 +5,7 @@ import com.trip.entity.Dishes;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,7 +15,10 @@ public class DishesService {
     @Resource
     DishesDao dishesDao;
 
-    public List<Dishes> queryByCondition(Map<String,Object> param){
+    public List<Dishes> queryByCondition(String keyWord,List<Integer> restaurantIds){
+        Map<String,Object> param=new HashMap();
+        param.put("dishesName",keyWord);
+        param.put("restaurantIdList",restaurantIds);
         return dishesDao.queryByCondition(param);
     }
 }
